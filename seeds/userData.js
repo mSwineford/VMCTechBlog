cacd08e6bf9aa50e9d27dece38535f0782188548
Matthew Swineford <m.swineford@yahoo.com>
2022-11-22 14:53:44 -0500

    added userData.js

---
+++ seeds/userData.js
@@ -0,0 +1,19 @@
+const sequelize = require("../config/connection");
+const {User, Post} = require("../models");
+
+const userdata = [
+    {
+        username: "Billy",
+        email: "billy@bob.email.com",
+        password: "supersecret12345"
+    },
+    {
+        username: "CoolGuy2004",
+        email: "cool@cool.email.com",
+        password: "password12345"
+    }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
