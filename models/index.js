f95bb167b399c7f0d2b8df47134c3f7a35904425
Matthew Swineford <m.swineford@yahoo.com>
2022-11-22 20:41:22 -0500

    added Models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = require("./Comment");
+const Comments = require("./Comments");
 
-User.hasMany(Post,{
-    foreignKey: "user_db"
+User.hasMany(Post, Comments, {
+    foreignKey: "user_id"
 });
-Post.belongsToMany(User, {
-    foreignKey: "user_db"
+Post.hasMany(Comments, {
+    foreignKey: "post_id"
 });
-Comment.belongsToMany(User, Post, {
-    foreignKey: "user_db"
+
+Post.belongsTo(User, {
+    foreignKey: "post_id"
+});
+Comments.belongsTo(User, Post, {
+    foreignKey: "post_id"
 });
